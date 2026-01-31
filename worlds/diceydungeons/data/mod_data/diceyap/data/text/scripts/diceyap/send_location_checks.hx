1d267d3ec6e1592a8980b3f57a7e26a9bbb3c0eb
Fylcoast <fylcoast@gmail.com>
2026-01-30 20:32:07 -0500

    Fix equipment loop in location check. List was shallow copying so removing in the loop was causing indexing issues...

--- worlds/diceydungeons/data/mod_data/diceyap/data/text/scripts/diceyap/send_location_checks.hx
+++ worlds/diceydungeons/data/mod_data/diceyap/data/text/scripts/diceyap/send_location_checks.hx
@@ -1,6 +1,7 @@
 // Loop through equipment and send any we picked up!
 //trace("[AP] {\"command\": \"send_equipment_debug\", \"payload\": \"" + self.equipment + "\"}");
 var equipment = self.equipment;
+var items_to_remove = [];
 
 for (item in equipment) {
   //trace("Item: " + item + ", isnull: " + (item == null));
@@ -14,6 +15,11 @@ for (item in equipment) {
     //trace("Attempting to send item: " + itemString);
     var location_id = itemString.split("[AP]")[1];
     trace("[AP] {\"command\": \"send_item\", \"payload\": \"" + location_id + "\"}");
-    self.equipment.remove(item);
+    //self.equipment.remove(item);
+    items_to_remove.push(item);
   }
+}
+
+for (item in items_to_remove) {
+  self.equipment.remove(item);
 }

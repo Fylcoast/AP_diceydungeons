3655ebf653179bd803d6066935d4a9f70ca63e91
Fylcoast <fylcoast@gmail.com>
2026-02-04 20:49:26 -0500

    Allow Episode 2 to give all equipment upgraded

---
+++ worlds/diceydungeons/data/mod_data/diceyap/data/text/scripts/diceyap/load_upgraded_level_items_by_category.hx
@@ -0,0 +1,25 @@
+// Incoming argument holds the item parameters we want.
+var gen = args[0];
+var episode = args[1];
+var list = args[2];
+var floor = args[3];
+var iter = args[4]; // number of the thing that is on the floor
+
+// AP data saved in columns name, generator, list 
+var apdata = loaddata("diceyap/ap_data");
+
+var res = [];
+
+//trace("Intended args: " + args);
+
+for (item in apdata) {
+  if (item.generator == gen && item.episode == episode && item.list == list && (!floor || !item.floor || item.floor == floor) && (!iter || !item.iter || item.iter == iter)) {
+    if (item.name.indexOf("Equipment") >= 0) {
+      res.push(item.name+"+");
+    } else {
+      res.push(item.name);
+    }
+  }
+}
+
+return res;
